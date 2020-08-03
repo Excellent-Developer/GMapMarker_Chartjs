120671099c86dfe4746d9be1ef9624c0f65489df
Excellent-Developer <fazylalmeeve@gmail.com>
2020-08-03 16:38:38 -0400

    organize

--- src/App.js
+++ src/App.js
@@ -9,38 +9,12 @@ import {
 } from "react-google-maps";
 import Geocode from "react-geocode";
 import { Bar } from "react-chartjs-2";
-import { getName } from "country-list";
+
+import arrangeCountryInfo from "./services/MapData";
 
 Geocode.setApiKey("AIzaSyBAjdJr2k1xu8VDmAi5ISmPkD2P9uvZCU0");
 Geocode.enableDebug();
 
-const countryInfo = require("./country.json");
-const countryGeocode = require("./countrycode-to-latlng.json");
-const arrangeCountryInfo = () => {
-  const countries = {};
-  const list = [];
-  countryInfo.hits.hits.map((item) => {
-    const geocode = countryGeocode[item._source.Country.toLocaleLowerCase()];
-    if (geocode) {
-      if (!countries[item._source.Country]) {
-        countries[item._source.Country] = {
-          lat: geocode[0],
-          lng: geocode[1],
-          name: getName(item._source.Country),
-          sources: [item._source],
-        };
-        list.push(item._source.Country);
-      } else {
-        countries[item._source.Country].sources = arraySourceByDate(
-          countries[item._source.Country].sources,
-          item._source
-        );
-      }
-    }
-  });
-  countries.list = list;
-  return countries;
-};
 const mapProps = {
   center: {
     lat: 37.773972,
@@ -49,17 +23,6 @@ const mapProps = {
   zoom: 4,
   key: "AIzaSyAyPqAkq2s9Z75QzarTAQHSHTxaNnVDqeE",
 };
-
-function arraySourceByDate(currentSources = [], newSource) {
-  for (let i = 0; i < currentSources.length; i++) {
-    if (currentSources[i].Date > newSource.Date) {
-      currentSources.splice(i, 0, newSource);
-      return currentSources;
-    }
-  }
-  currentSources.splice(currentSources.length - 1, 0, newSource);
-  return currentSources;
-}
 
 const MapWithAMakredInfoWindow = compose(
   withScriptjs,
